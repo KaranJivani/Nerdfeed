8fe13314449cb02382b35a9886dccef0a956887f
Karan Jivani <karanjivani@gmail.com>
2016-08-02 13:32:32 -0500

    Added web service call to get courses and reloading table getting main queue  after getting response

--- NerdFeed/KRNCoursesViewController.m
+++ NerdFeed/KRNCoursesViewController.m
@@ -10,13 +10,48 @@
 
 @interface KRNCoursesViewController ()<UITableViewDataSource,UITableViewDelegate>
 
+@property (nonatomic) NSURLSession *session;
+@property(nonatomic,copy) NSArray *courses;
+
 @end
 
 @implementation KRNCoursesViewController
 
+-(instancetype)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        self.navigationItem.title = @"Courses";
+        
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
+    }
+    [self fetchFeed];
+    return self;
+}
+
+-(void) fetchFeed {
+    
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        self.courses = [jsonObject objectForKey:@"courses"];
+        NSLog(@"%@",self.courses);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
+    
+    [dataTask resume];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,18 +59,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
 #pragma mark Tableview DataSource Methods
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 0;
+    return [self.courses count];
 }
 
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    NSDictionary *course = [self.courses objectAtIndex:indexPath.row];
+    cell.textLabel.text = [course valueForKey:@"title"];
     
-    return nil;
+    return cell;
 }
 
 #pragma mark Tableview Delegate Methods
